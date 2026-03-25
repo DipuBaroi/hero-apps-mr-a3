b2a403dcdc6f198d81596c3bcfd89eaf1bbdfcac
DipuBaroi <dipubaroi758@gmail.com>
2026-03-25 23:14:46 +0600

    Create error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import errorImg from '../assets/error-404.png'
+import { Link } from 'react-router';
 
 const ErrorPage = () => {
     return (
         <div>
-            <h3 className="text-xl text-center">Page not found</h3>
+            <div className='text-center min-h-screen space-y-4'>
+                <div className='flex justify-center pt-5'>
+                    <img src={errorImg} alt="" />
+                </div>
+                <h2 className='text-4xl font-bold text-[#001931]'>Oops, page not found!</h2>
+                <p className='text-[#627382]'>The page you are looking for is not available.</p>
+                
+                <Link to={'./'} className=' btn text-white rounded-lg bg-linear-to-r from-[#632EE3] to-[#9F62F2]'> Go Back </Link>
+            </div>
         </div>
     );
 };
